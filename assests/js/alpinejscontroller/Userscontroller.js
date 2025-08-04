bf0886a59b94c1626b113e0b56cdf6715bdfb6f2
Mahdi <mirdamadians@gmail.com>
2025-08-04 14:27:49 +0330

    part #17

--- assests/js/alpinejscontroller/Userscontroller.js
+++ assests/js/alpinejscontroller/Userscontroller.js
@@ -2,17 +2,38 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('usersData',function(){
         return{
             users: [],
+            pageUsers: [],
             isLoading: false,
             showAddModal: false,
+            pageCount: 1,
+            itemsCount: 4,
+            currentPage: 1,
             getUsers(){
                 this.isLoading = true
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                     this.users = res.data
+                    this.pagination()
                 }).catch(error=>{
                     console.log(error.message)
                 }).finally(()=>{
                     this.isLoading = false
                 })
+            },
+            pagination(){
+                this.pageCount = Math.ceil(this.users.length / this.itemsCount) // 10 / 4 = 3
+                const start = (this.currentPage * this.itemsCount) - this.itemsCount // 0
+                const end = this.currentPage * this.itemsCount // 4
+                this.pageUsers = this.users.slice(start,end)
+            },
+            nextPage(){
+                this.currentPage++
+                if (this.currentPage > this.pageCount) this.currentPage = this.pageCount
+                this.pagination()
+            },
+            prevPage(){
+                this.currentPage--
+                if (this.currentPage < 1) this.currentPage = 1
+                this.pagination()
             }
         }
     })
