4c8614d312ee0e08ffaca16789a28ab68071f19b
Mahdi <mirdamadians@gmail.com>
2025-08-04 11:43:39 +0330

    part #16

--- assests/js/alpinejscontroller/Userscontroller.js
+++ assests/js/alpinejscontroller/Userscontroller.js
@@ -1,11 +1,19 @@
-    document.addEventListener('alpine:init', () => {
-        Alpine.data('mainData', () => ({
-            open: false,
- 
-            message: 'I love You!',
-            names: ['qasem', 'abbas', 'nima'],
-            testFunc(){
-               alert(this.message)
+document.addEventListener('alpine:init', () => {
+    Alpine.data('usersData',function(){
+        return{
+            users: [],
+            isLoading: false,
+            showAddModal: false,
+            getUsers(){
+                this.isLoading = true
+                axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
+                    this.users = res.data
+                }).catch(error=>{
+                    console.log(error.message)
+                }).finally(()=>{
+                    this.isLoading = false
+                })
             }
-        }))
+        }
     })
+})
